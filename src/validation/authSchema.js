9888ce74f476ee2dc157fe00e918936fde23c552
lamlqb <lamlqb@bap.jp>
2023-07-21 11:53:12 +0700

    fix auth schema

--- src/validation/authSchema.js
+++ src/validation/authSchema.js
@@ -3,6 +3,7 @@ const Joi = require("@hapi/joi");
 const authSchema = Joi.object({
   email: Joi.string().email().lowercase().required(),
   password: Joi.string().min(6).max(16).required(),
+  user_name: Joi.string(),
 });
 
 module.exports = authSchema;
